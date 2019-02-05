0b670cbf1060f7d91cd393a65adcae69dd103cf2
riagoncalves <ricardo.goncalves252@gmail.com>
2019-02-05 22:09:33 +0000

    added playing action to bot defined only by bot owner

--- main.rb
+++ main.rb
@@ -11,11 +11,26 @@ isa = Discordrb::Commands::CommandBot.new(token: CONFIG['token'],
                                           client_id: CONFIG['client_id'],
                                           prefix: CONFIG['prefix'])
 
-puts "This bot's invite URL is #{isa.invite_url}."
+puts "#{isa.bot_application.name}'s invite URL is #{isa.invite_url}."
 puts 'Click on it to invite it to your server.'
 
 isa.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
+end
+
+isa.command :user do |event|
+  event.user.name
+end
+
+isa.command :play do |_event, *args|
+  if isa.bot_application.owner.id == _event.user.id
+    if args.length > 0
+      isa.game = args.join(' '); nil
+      "I'm playing #{isa.profile.game}";
+    end
+  else
+    "You don't have permissions to do this!"
+  end
 end
 
 isa.run
