63d541b25e3434fac27941dcc447c09e1ff6af30
riagoncalves <ricardo.goncalves252@gmail.com>
2019-02-05 19:26:52 +0000

    changed the bot to command bot, added client_id and prefix

--- main.rb
+++ main.rb
@@ -7,7 +7,9 @@ puts 'All dependencies loaded'
 CONFIG = YAML.load_file('config/config.yaml')
 puts 'Config loaded from file'
 
-isa = Discordrb::Bot.new token: CONFIG['token']
+isa = Discordrb::Commands::CommandBot.new(token: CONFIG['token'],
+                                          client_id: CONFIG['client_id'],
+                                          prefix: CONFIG['prefix'])
 
 puts "This bot's invite URL is #{isa.invite_url}."
 puts 'Click on it to invite it to your server.'
