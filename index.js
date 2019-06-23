f166ec85664334ed9f85980b84795eef51d16a49
riagoncalves <ricardo.goncalves252@gmail.com>
2019-06-23 18:41:26 +0100

    prevent bot to answer commands in dm

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	
-	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+	if (!msg.content.startsWith(prefix) || msg.author.bot || msg.channel.type === 'dm') return;
 
 	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
