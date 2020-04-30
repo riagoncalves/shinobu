2efd88bc85fd4f705d5b5af3b67898deea141256
unknown <ricardo.goncalves252@gmail.com>
2020-04-30 20:41:30 +0100

    purge command

--- bot/commands.js
+++ bot/commands.js
@@ -317,6 +317,17 @@ const commands = {
 			});
 		},
 	},
+	'purge': {
+		desc: function(prefix) {
+			return `Write \`${prefix}purge <number>\` to delete up to 100 messages`;
+		},
+		process: function(client, msg, args) {
+			if(!msg.member.hasPermission('ADMINISTRATOR') || !msg.member.hasPermission('MANAGE_GUILD') || !msg.member.hasPermission('MANAGE_MESSAGES') || !msg.member.hasPermission('MANAGE_CHANNELS') || !(msg.author.id === owner)) return msg.reply('You don\'t have permissions to do that!');
+			const deleteCount = parseInt(args[0], 10) + 1;
+			if(!deleteCount || deleteCount < 2 || deleteCount > 100) return msg.reply('Please provide a number between 2 and 100 for the number of messages to delete');
+			msg.channel.bulkDelete(deleteCount);
+		},
+	},
 };
 
 module.exports = function(cmd, client, msg, args, prefix) {
