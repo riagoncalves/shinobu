064040d266dde015ab163d6cf5a308649caefbc4
unknown <ricardo.goncalves252@gmail.com>
2020-05-01 16:35:31 +0100

    db host as env vars
    remove excessive prod info
    
    sequelize config case use_env_variable exists

--- db/config.js
+++ db/config.js
@@ -5,15 +5,11 @@ module.exports = {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PW,
 		database: process.env.DB_NAME,
-		host: '127.0.0.1',
+		host: process.env.DB_HOST,
 		dialect: 'postgres',
 	},
 	production: {
 		use_env_variable: 'DATABASE_URL',
-		username: process.env.DB_USER,
-		password: process.env.DB_PW,
-		database: process.env.DB_NAME,
-		host: '127.0.0.1',
 		dialect: 'postgres',
 	},
 };
--- db/db.js
+++ db/db.js
@@ -2,4 +2,4 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config.js')[env];
 
-module.exports = new Sequelize(config.database, config.username, config.password, config);
+module.exports = config.use_env_variable ? new Sequelize(process.env[config.use_env_variable], config) : new Sequelize(config.database, config.username, config.password, config);
