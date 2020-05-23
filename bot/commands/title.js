c71a657b9c0ca2dc6ac6253b537cb23e1e357f21
unknown <ricardo.goncalves252@gmail.com>
2020-05-23 15:03:28 +0100

    title command

---
+++ bot/commands/title.js
@@ -0,0 +1,25 @@
+const models = require('../../db/models');
+
+module.exports = {
+	desc: function(prefix) {
+		return `Write \`${prefix}title\` to see your own title and \`${prefix}title <title>\` to set a new title.`;
+	},
+	process: async function(client, msg, args) {
+		const user = await models.User.findOne({ where: { userID: msg.author.id } });
+
+		if(args.length > 0) {
+			const title = args.join(' ');
+
+			if(title.length > 30) return msg.channel.send(`**${msg.author.username}** your title is too long, choose another one!`);
+
+			user.update({
+				title: title,
+			});
+			return msg.channel.send(`**${msg.author.username}** your new title is **${title}**!`);
+		}
+		else {
+			const title = user.title == null ? 'Empty Head' : user.title;
+			return msg.channel.send(`**${msg.author.username}** your title is **${title}**!`);
+		}
+	},
+};
