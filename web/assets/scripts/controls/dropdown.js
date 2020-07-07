adea3be3926f1505f537d3cb9b38323d4afe3715
unknown <ricardo.goncalves252@gmail.com>
2020-07-08 00:30:45 +0100

    dropdown menu adjustment

--- web/assets/scripts/controls/dropdown.js
+++ web/assets/scripts/controls/dropdown.js
@@ -3,10 +3,19 @@ export default {
 		const btn = el.querySelector('.user-dropdown');
 		const dropdownMenu = el.querySelector('.user-dropdown-menu');
 		const arrowDown = btn.querySelector('.arrow-down');
+
 		btn.addEventListener('click', (e) => {
-			e.preventDefault();
+			e.stopPropagation();
 			dropdownMenu.classList.toggle('active');
 			arrowDown.classList.toggle('active');
+		});
+
+		window.addEventListener('click', (e) => {
+			e.stopPropagation();
+			if (dropdownMenu.classList.contains('active')) {
+				dropdownMenu.classList.toggle('active');
+				arrowDown.classList.toggle('active');
+			}
 		});
 	},
 };
