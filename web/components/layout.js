26660731481012d7744d7053de4d5a730346efbe
unknown <ricardo.goncalves252@gmail.com>
2021-02-07 01:22:26 +0000

    remove comma

--- web/components/layout.js
+++ web/components/layout.js
@@ -32,10 +32,10 @@ const Layout = (props) => (
 				dangerouslySetInnerHTML={{
 					__html:
 						`window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
+							function gtag(){dataLayer.push(arguments)}
 							gtag('js', new Date());
 
-							gtag('config', '${process.env.googleTag}');`,
+							gtag('config', '${process.env.googleTag}');`
 				}}
 			/>
 		</Head>
