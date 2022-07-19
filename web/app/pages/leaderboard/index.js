d03642ce68d72307319cc129ae404f81843c7360
Ricardo Gonçalves <ricardo.goncalves252@gmail.com>
2022-07-19 23:08:52 +0100

    feat(website): leaderboard page

--- web/app/pages/leaderboard/index.js
+++ web/app/pages/leaderboard/index.js
@@ -45,17 +45,8 @@ export default class Profile extends React.Component {
         <section className="flex relative w-full h-full mt-8 flex-col">
           <div className="flex flex-col items-center">
             <h2 className="font-main text-white font-extrabold text-2xl px-8 pb-16 pt-20">Leaderboard</h2>
-            <div className="flex w-4/5 max-w-2xl items-center py-3.5">
-              <div className="flex items-center flex-1">
-                <p className="font-main text-md text-white font-bold">User</p>
-              </div>
-              <div>
-                <p className="font-main text-md text-white font-bold">Level</p>
-              </div>
-              <span className='w-24'></span>
-            </div>
-            {props.users.map((appUser) => (
-              <LeaderboardUser key={appUser.userID} user={appUser} />
+            {props.users.map((appUser, index) => (
+              <LeaderboardUser key={appUser.userID} rank={index + 1} user={appUser} />
             ))}
           </div>
         </section>
